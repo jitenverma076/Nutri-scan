de4394f749aa1c03546274cac9867a8669a810a0
pragatisharma21 <pragatis212@gmail.com>
2025-01-19 13:41:55 +0530

    api route update

--- src/Api/apiStore.js
+++ src/Api/apiStore.js
@@ -19,16 +19,16 @@ apiClient.interceptors.request.use(
 );
 
 const loginUser = (data) => {
-  return apiClient.post("/api/user/login", data);
+  return apiClient.post("/user/login", data);
 };
 
 const signupUser = (data) => {
-  return apiClient.post("/api/user/signup", data);
+  return apiClient.post("/user/signup", data);
 };
 
 
 const addDish = (data)=>{
-    return apiClient.post("/api/dish/addDish", data)
+    return apiClient.post("/dish/addDish", data)
 }
 
 
